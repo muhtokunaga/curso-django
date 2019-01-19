fa53dd81f180bcb2dab565c6c06be4f21fb30fd5
Murilo Tokunaga <murilo.tokunaga@yahoo.com.br>
2019-01-19 21:00:04 -0200

    Inclusao Variavel de Debug

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rb&(9o+e7ix^-us(bz4o=f_))5i_(y)idk)@1^!km=m_5(23a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
